086a8056b81a02151ffd5e29e8272f31ca08c7b0
Flammrock <flammrockgame@gmail.com>
2019-03-10 23:36:46 +0100

    Create canvas.js

---
+++ src/towerdefenceengine/system/canvas.js
@@ -0,0 +1,26 @@
+CanvasRenderingContext2D.prototype.roundRect = function(x, y, width, height, radius) {
+  if (typeof radius === 'undefined') {
+    radius = 5;
+  }
+  if (typeof radius === 'number') {
+    radius = {tl: radius, tr: radius, br: radius, bl: radius};
+  } else {
+    var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+    for (var side in defaultRadius) {
+      radius[side] = radius[side] || defaultRadius[side];
+    }
+  }
+  if (width > radius.tl) {
+    this.beginPath();
+    this.moveTo(x + radius.tl, y);
+    this.lineTo(x + width - radius.tr, y);
+    this.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    this.lineTo(x + width, y + height - radius.br);
+    this.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    this.lineTo(x + radius.bl, y + height);
+    this.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    this.lineTo(x, y + radius.tl);
+    this.quadraticCurveTo(x, y, x + radius.tl, y);
+    this.closePath();
+  }
+}
